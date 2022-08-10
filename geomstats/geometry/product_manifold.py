462a3ac529ee68209c49fef99842d699436ba667
Nicolas Fishman <fishman@zizgpu04.cpu.stats.ox.ac.uk>
2022-08-10 16:53:48 +0100

    Completely functional product manifold

--- geomstats/geometry/product_manifold.py
+++ geomstats/geometry/product_manifold.py
@@ -17,178 +17,16 @@ import jax.numpy as jnp
 from geomstats.geometry.product_riemannian_metric import ProductSameRiemannianMetric
 from jax.scipy.linalg import block_diag
 
-from functools import partial
-from jax import jit
-
-from collections import namedtuple
-
-EmbeddingSpace = namedtuple("EmbeddingSpace", "dim")
-
-@partial(jit, static_argnums=[0, 1, 2])
-def pull(i, cum_dims, manifold_dims, value):
-    return jax.lax.slice_in_dim(
-        value, cum_dims[i] - manifold_dims[i], self.cum_dims[i], axis=-1
-    )
-
-class ProductSameManifold(Manifold):
-    def __init__(self, manifold, mul, metric=None, default_point_type="vector", **kwargs):
-        self.manifold = manifold
-        self.mul = mul
-        self.dim = self.mul * self.manifold.dim
-
-        if metric is None:
-            metric = ProductSameRiemannianMetric(self.manifold.metric, self.mul)
-        self.metric = metric
-
-        super(ProductSameManifold, self).__init__(
-            dim=self.dim, metric=metric, default_point_type=default_point_type, **kwargs
-        )
-        self.dim = self.mul * self.manifold.dim
-        if isinstance(manifold, EmbeddedManifold):
-            self.embedding_space = ProductSameManifold(manifold.embedding_space, mul)
-
-    def _iterate_over_manifolds(self, func, kwargs, in_axes=-2, out_axes=-2):
-        method = getattr(self.manifold, func)
-        in_axes = []
-        args_list = []
-        for key, value in kwargs.items():
-            if hasattr(value, "reshape"):
-                # value : shape=[..., mul*dim] -> shape=[..., mul, dim]
-                value = value.reshape((*value.shape[:-1], self.mul, -1))
-                in_axes.append(-2)
-            else:
-                in_axes.append(None)
-            args_list.append(value)
-        # out = jax.vmap(method, in_axes=in_axes, out_axes=out_axes)(**args)
-        # NOTE: Annoyingly cannot pass named arguments with in_axes! https://github.com/google/jax/issues/7465
-        out = jax.vmap(method, in_axes=in_axes, out_axes=out_axes)(*args_list)
-        out = out.reshape((*out.shape[:out_axes], -1))
-        # value : shape=[..., mul, dim] -> shape=[..., mul*dim]
-        return out
-
-    def belongs(self, point, atol=gs.atol):
-        belongs = self._iterate_over_manifolds(
-            "belongs",
-            {"point": point, "atol": atol},
-            out_axes=-1,
-        )
-        belongs = gs.all(belongs, axis=-1)
-        return belongs
-
-    def projection(self, point):
-        projected_point = self._iterate_over_manifolds(
-            "projection",
-            {"point": point},
-        )
-        return projected_point
-
-    def to_tangent(self, vector, base_point):
-        tangent_vec = self._iterate_over_manifolds(
-            "to_tangent",
-            {"vector": vector, "base_point": base_point},
-        )
-        return tangent_vec
-
-    def random_uniform(self, state, n_samples=1):
-        if self.mul > 1:
-            new_state = jax.random.split(state, num=self.mul)
-            state = new_state.reshape((-1))
-        samples = self._iterate_over_manifolds(
-            "random_uniform",
-            {"state": state, "n_samples": n_samples},
-        )
-        return samples
-
-    def random_point(self, n_samples=1, bound=1.0):
-        samples = self._iterate_over_manifolds(
-            "random_point",
-            {"n_samples": n_samples, "bound": bound},
-        )
-        return samples
-
-    def random_walk(self, rng, x, t):
-        if self.mul > 1:
-            new_state = jax.random.split(rng, num=self.mul)
-            rng = new_state.reshape((-1))
-        if isinstance(x, jax.numpy.ndarray):
-            t = t[:, None] * jnp.ones(self.mul)[None, :]
-        walks = self._iterate_over_manifolds("random_walk", {"rng": rng, "x": x, "t": t})
-        return walks
-
-    def _log_heat_kernel(self, x0, x, t, n_max):
-        t = t * jnp.ones((*x0.shape[:-1], self.mul))
-
-        log_probs = self._iterate_over_manifolds(
-            "_log_heat_kernel", {"x0": x0, "x": x, "t": t, "n_max": n_max}, out_axes=-1
-        )
-        return jnp.sum(log_probs, axis=-1)
-
-    def random_normal_tangent(self, state, base_point, n_samples=1):
-        size = (n_samples, self.mul * self.manifold.embedding_space.dim)
-        state, ambiant_noise = gs.random.normal(state=state, size=size)
-        return state, self.to_tangent(vector=ambiant_noise, base_point=base_point)
-
-    def is_tangent(self, vector, base_point, atol=gs.atol):
-        is_tangent = self._iterate_over_manifolds(
-            "is_tangent",
-            {"vector": vector, "base_point": base_point, "atol": atol},
-            out_axes=-1,
-        )
-        is_tangent = gs.all(is_tangent, axis=-1)
-        return is_tangent
-
-    def exp(self, tangent_vec, base_point, **kwargs):
-        return self._iterate_over_manifolds(
-            "exp",
-            {"tangent_vec": tangent_vec, "base_point": base_point},
-        )
-
-    def log(self, point, base_point, **kwargs):
-        return self._iterate_over_manifolds(
-            "log",
-            {"point": point, "base_point": base_point},
-        )
-
-    def div_free_generators(self, x):
-        generators = self._iterate_over_manifolds("div_free_generators", {"x": x})
-        # return generators
-        def block_diag_generators(generators):
-            gens = jnp.split(generators, self.mul, axis=1)
-            return block_diag(*gens)
-
-        block_diag_generators = jax.vmap(block_diag_generators, in_axes=0, out_axes=0)
-        return block_diag_generators(generators)
-
-    @property
-    def log_volume(self):
-        # TODO: Double check
-        return self.mul * self.manifold.log_volume
-
-    # TODO: I CBA to write product groups rn, this is a hacky af way to get the isom_group.dim thing to work for moser flows
-
-    @property
-    def isom_group(self):
-        class dotdict(dict):
-            """dot.notation access to dictionary attributes"""
-
-            __getattr__ = dict.get
-            __setattr__ = dict.__setitem__
-            __delattr__ = dict.__delitem__
-
-        return dotdict(dim=self.manifold.isom_group.dim * self.mul)
-
-    
-class ProductManifold(Manifold):
+class NonEmbeddedProductManifold(Manifold):
     def __init__(self, dim=None, manifolds=None, metric=None, default_point_type="vector", **kwargs):
         self.manifolds = manifolds
-        print(manifolds)
-        self.dim = sum([manifold.dim for manifold in self.manifolds]) if dim is None else dim
+        self.dim = sum([manifold.dim for manifold in self.manifolds])
 
         if metric is None:
             metric = ProductRiemannianMetric([manifold.metric for manifold in self.manifolds])
         self.metric = metric
 
-        super(ProductManifold, self).__init__(
+        super(NonEmbeddedProductManifold, self).__init__(
             dim=self.dim, metric=metric, default_point_type=default_point_type, **kwargs
         )
         self.manifold_dims = [manifold.dim for manifold in self.manifolds]
@@ -201,13 +39,14 @@ class ProductManifold(Manifold):
             manifold_kwargs = {}
             for key, value in kwargs.items():
                 if hasattr(value, "reshape"):
-                    if value.shape[-1] == len(self.manifolds):
+                    if value.shape[0] == len(self.manifolds):
+                        manifold_kwargs[key] = value[i, ...]
+                    elif value.shape[-1] == len(self.manifolds):
                         manifold_kwargs[key] = value[..., i]
                     else:
                         manifold_kwargs[key] = value[..., (self.cum_dims[i] - self.manifold_dims[i]):self.cum_dims[i]]
                 else:
                     manifold_kwargs[key] = values
-                print(manifold_kwargs[key])
             out.append(getattr(manifold, func)(**manifold_kwargs))
         if None in out:
             return None
@@ -239,7 +78,7 @@ class ProductManifold(Manifold):
     def random_uniform(self, state, n_samples=1):
         if len(self.manifolds) > 1:
             new_state = jax.random.split(state, num=len(self.manifolds))
-            state = new_state.reshape((-1))
+            state = new_state# .reshape((-1))
         samples = self._iterate_over_manifolds(
             "random_uniform",
             {"state": state, "n_samples": n_samples},
@@ -256,7 +95,7 @@ class ProductManifold(Manifold):
     def random_walk(self, rng, x, t):
         if len(self.manifolds) > 1:
             new_state = jax.random.split(rng, num=len(self.manifolds))
-            rng = new_state# .reshape((-1))
+            rng = new_state # .reshape((-1))
         if isinstance(x, jax.numpy.ndarray):
             t = t[:, None] * jnp.ones(len(self.manifolds))[None, :]
         walks = self._iterate_over_manifolds("random_walk", {"rng": rng, "x": x, "t": t})
@@ -324,31 +163,47 @@ class ProductManifold(Manifold):
 
         return dotdict(dim=sum([manifold.isom_group.dim for manifold in self.manifolds]))
 
-class EmbeddedProductManifold(ProductManifold, EmbeddedManifold):
-    def __init__(self, manifolds, metric=None, default_point_type="vector", **kwargs):
+class ProductManifold(NonEmbeddedProductManifold, EmbeddedManifold):
+    def __init__(self, manifolds, metric=None, embedding_space=None, default_point_type="vector", **kwargs):
         self.manifolds = manifolds
         manifold_dims = [manifold.embedding_space.dim if hasattr(manifold, "embedding_space")
-                              else manifold.dim for manifold in self.manifolds]
+                         else manifold.dim for manifold in self.manifolds]
         dim = sum(manifold_dims)
-        embedding_space = ProductManifold(
-            dim=dim, # wrong dim but itll be fixed
-            manifolds = [
-                manifold.embedding_space if hasattr(manifold, "embedding_space") 
-                else manifold for manifold in self.manifolds        
-            ]
-        )
-        super(EmbeddedProductManifold, self).__init__(
+        if embedding_space is None:
+            embedding_space = NonEmbeddedProductManifold(
+                dim=dim, # wrong dim but itll be fixed
+                manifolds = [
+                    manifold.embedding_space if hasattr(manifold, "embedding_space") 
+                    else manifold for manifold in self.manifolds        
+                ]
+            ) 
+        super(ProductManifold, self).__init__(
             dim=dim,
             manifolds=manifolds,
-            embedding_space=embedding_space,
+            embedding_space=embedding_space, 
+            metric=metric,
             **kwargs
         )
         self.manifold_dims = manifold_dims
         self.dim = dim
-        print("TASA", dim)
+        self.cum_dims = np.cumsum(self.manifold_dims).tolist()
     
+class ProductSameManifold(ProductManifold):
+    def __init__(self, manifold, mul, metric=None, default_point_type="vector", **kwargs):
+        self.manifold = manifold
+        self.mul = mul
+        self.dim = self.mul * (manifold.embedding_space.dim if hasattr(manifold, "embedding_space") else manifold.dim)
+        if metric is None:
+            metric = ProductSameRiemannianMetric(self.manifold.metric, self.mul)
+        self.metric = metric
+
+        super(ProductSameManifold, self).__init__(
+            manifolds=self.mul * [self.manifold], 
+            metric=metric, 
+            default_point_type=default_point_type, **kwargs
+        )
     
-class Product2Manifolds(EmbeddedProductManifold):
+class Product2Manifolds(ProductManifold):
     def __init__(
         self, manifold_a, manifold_b, **kwargs
     ):
--- geomstats/geometry/product_riemannian_metric.py
+++ geomstats/geometry/product_riemannian_metric.py
@@ -18,71 +18,6 @@ from jax import numpy as jnp
 
 import numpy as np
 
-class ProductSameRiemannianMetric(RiemannianMetric):
-    """Jax based vmapped product metircs over the same manifold"""
-
-    def __init__(self, metric, mul, default_point_type="vector", **kwargs):
-        self.metric = metric
-        self.mul = mul
-        self.dim = self.mul * self.metric.dim
-
-        super().__init__(
-            dim=self.dim,
-            signature=(metric.signature[0] * self.mul, metric.signature[1] * self.mul),
-            default_point_type=default_point_type,
-        )
-
-    def _iterate_over_metrics(self, func, kwargs, in_axes=-2, out_axes=-2):
-        method = getattr(self.metric, func)
-        in_axes = []
-        args_list = []
-        for key, value in kwargs.items():
-            if hasattr(value, "reshape"):
-                # value : shape=[..., mul*dim] -> shape=[..., mul, dim]
-                value = value.reshape((*value.shape[:-1], self.mul, -1))
-                in_axes.append(-2)
-            else:
-                in_axes.append(None)
-            args_list.append(value)
-        # out = jax.vmap(method, in_axes=in_axes, out_axes=out_axes)(**args)
-        # NOTE: Annoyingly cannot pass named arguments with in_axes! https://github.com/google/jax/issues/7465
-        out = jax.vmap(method, in_axes=in_axes, out_axes=out_axes)(*args_list)
-        out = out.reshape((*out.shape[:out_axes], -1))
-        # value : shape=[..., mul, dim] -> shape=[..., mul*dim]
-        return out
-
-    def exp(self, tangent_vec, base_point):
-        return self._iterate_over_metrics(
-            "exp",
-            {
-                "tangent_vec": tangent_vec,
-                "base_point": base_point,
-            },
-        )
-
-    def log(self, point, base_point=None, point_type=None):
-        return self._iterate_over_metrics(
-            "log",
-            {
-                "point": point,
-                "base_point": base_point,
-                # "point_type": point_type,
-            },
-        )
-
-    def squared_norm(self, vector, base_point=None):
-        return gs.sum(
-            self._iterate_over_metrics(
-                "squared_norm",
-                {
-                    "vector": vector,
-                    "base_point": base_point,
-                },
-                out_axes=-1,
-            ),
-            axis=-1,
-        )
-
 
 class ProductRiemannianMetric(RiemannianMetric):
     """Jax based vmapped product metircs over the same manifold"""
@@ -90,15 +25,12 @@ class ProductRiemannianMetric(RiemannianMetric):
     def __init__(self, metrics, default_point_type="vector", **kwargs):
         self.metrics = metrics
         self.metric_dims = [
-            metric.embedding_space.dim if hasattr(metric, "embedding_space") 
+            metric.embedding_metric.dim if hasattr(metric, "embedding_metric") 
             else metric.dim for metric in self.metrics
         ]
-        self.cum_dims = np.cumsum(
-            [metric.embedding_space.dim if hasattr(metric, "embedding_space") 
-             else metric.dim for metric in self.metrics]
-        ).tolist()
+        
+        self.cum_dims = np.cumsum(self.metric_dims).tolist()
         self.dim = sum(self.metric_dims)
-
         super().__init__(
             dim=self.dim,
             signature=(
@@ -110,6 +42,7 @@ class ProductRiemannianMetric(RiemannianMetric):
 
     def _iterate_over_metrics(self, func, kwargs, in_axes=-2, out_axes=-2):
         out = []
+        print("metric", func, self.metrics)
         for i, metric in enumerate(self.metrics):
             metric_kwargs = {}
             for key, value in kwargs.items():
@@ -151,17 +84,19 @@ class ProductRiemannianMetric(RiemannianMetric):
             axis=-1,
         )
     
-class ProductSameRiemannianMetric2(ProductRiemannianMetric):
+class ProductSameRiemannianMetric(ProductRiemannianMetric):
     """Jax based vmapped product metircs over the same manifold"""
 
     def __init__(self, metric, mul, default_point_type="vector", **kwargs):
+        
         self.metric = metric
         self.mul = mul
-
         super().__init__(
             metrics=self.mul * [self.metric],
             default_point_type=default_point_type,
         )
+        if hasattr(self.metric, "embedding_metric"):
+            self.embedding_metric = ProductSameRiemannianMetric(self.metric.embedding_metric, mul)
 
     def _iterate_over_metrics(self, func, kwargs, in_axes=-2, out_axes=-2):
         method = getattr(self.metric, func)
@@ -169,20 +104,14 @@ class ProductSameRiemannianMetric2(ProductRiemannianMetric):
         args_list = []
         for key, value in kwargs.items():
             if hasattr(value, "reshape"):
-                # value : shape=[..., mul*dim] -> shape=[..., mul, dim]
                 value = value.reshape((*value.shape[:-1], self.mul, -1))
                 in_axes.append(-2)
             else:
                 in_axes.append(None)
             args_list.append(value)
-        # out = jax.vmap(method, in_axes=in_axes, out_axes=out_axes)(**args)
-        # NOTE: Annoyingly cannot pass named arguments with in_axes! https://github.com/google/jax/issues/7465
         out = jax.vmap(method, in_axes=in_axes, out_axes=out_axes)(*args_list)
         out = out.reshape((*out.shape[:out_axes], -1))
-        # value : shape=[..., mul, dim] -> shape=[..., mul*dim]
         return out
 
     
-        
-        
         
