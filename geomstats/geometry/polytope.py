ac1e01b379f53e887f4bf1244e466beb2cc0ff5a
Nicolas Fishman <fishman@zizgpu04.cpu.stats.ox.ac.uk>
2022-10-07 12:18:26 +0100

    updates to polytope conversion code, not final

--- geomstats/geometry/polytope.py
+++ geomstats/geometry/polytope.py
@@ -108,6 +108,74 @@ def reflect(r, sn, T, b, eps=1e-6, eps2=1e-10, max_val=1e10, pass_by_value=True,
     return rp
 
 
+def to_params(x):
+    # this is jittable
+    diag = x[1:, :] - x[0, :]
+    l = gs.sqrt(gs.sum((x[1:-1] - x[2:])**2, axis=1))
+    d = gs.sqrt(gs.sum((x[0]-x[-1])**2))
+    r = gs.sqrt(gs.sum((diag)**2, axis=1))
+    diag /= r[:, None]
+
+    n = gs.cross(diag[:-1, :], diag[1:, :])
+    n /= gs.sqrt(gs.sum(n**2, axis=1))[:, None]
+
+    tau = []
+    for j in range(n.shape[0] - 1):
+        theta = gs.sign(n[j + 1] @ diag[j]) * gs.arccos(n[j + 1] @ n[j])
+        tau.append(theta)
+    tau = gs.array(tau)
+    return r, tau, l, d
+
+def to_euclidean(r, tau, l, x0, xn, xn1):
+    # not jittable
+    m = r.shape[0] + 1
+
+    diag = gs.ones((m - 1, 3))
+    n = gs.ones((m - 2, 3))
+    gamma = gs.ones(m-3)
+
+    diag[m-2] = xn - x0
+    diag[m-3] = xn1 - x0
+    diag /= gs.sqrt(gs.sum((diag)**2, axis=1))[:, None]
+
+    n[m-3] = gs.cross(diag[-1], diag[-2])
+    n /= gs.sqrt(gs.sum((n)**2, axis=1))[:, None]
+    
+    x = gs.zeros((m, 3))
+    x[0], x[m-2], x[m-1] = x0, xn1, xn
+
+    for j in range(m-2-2, -1, -1):
+        n[j] = diag[j + 1] * (diag[j + 1] @ n[j+1]) + \
+               gs.cos(-tau[j]) * gs.cross(gs.cross(diag[j + 1], n[j+1]), diag[j + 1]) + \
+               gs.sin(-tau[j]) * gs.cross(diag[j + 1], n[j+1])
+        n /= gs.sqrt(gs.sum((n)**2, axis=1))[:, None]
+        gamma[j] = gs.arccos((r[j]**2 + r[j+1]**2 - l[j]**2) / (2 * r[j] * r[j+1]))
+        diag[j] = n[j] * (n[j] @ diag[j+1]) + \
+                  gs.cos(gamma[j]) * gs.cross(gs.cross(n[j], diag[j+1]), n[j]) + \
+                  gs.sin(gamma[j]) * gs.cross(n[j], diag[j+1])
+        diag /= gs.sqrt(gs.sum((diag)**2, axis=1))[:, None]
+        x[j+1] = x0 + r[j] * diag[j]
+    return x
+
+def get_constraints(l, D):
+    m = l.shape[0] + 2
+    T = np.zeros((3 * m - 8, m - 3))
+    b = np.zeros((3 * m - 8))
+    T[0, 0], b[0] = 1, l[0] + l[1]
+    T[1, 0], b[1] = -1, -gs.abs(l[0] - l[1])
+    i = 2
+    k = 1
+    for j in range(0, m-4):
+        T[i, j], T[i, j + 1], b[i] = 1, -1, l[j + k]
+        T[i + 1, j], T[i + 1, j + 1], b[i + 1] = -1, 1, l[j + k]
+        T[i + 2, j], T[i + 2, j + 1], b[i + 2] = -1, -1, -l[j + k]
+        i += 3
+    T[-2, -1], b[-2] = 1, l[-3] + D
+    T[-1, -1], b[-1] = -1, - gs.abs(l[-3] - D)
+    return T, b
+
+
+
 class Polytope(Euclidean):
     """Class for Euclidean spaces.
 
