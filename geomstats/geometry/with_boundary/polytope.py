ca4b21b8103e24283368c12cac2653e9a0c3a4de
njwfish <njwfish@gmail.com>
2022-10-20 12:04:12 +0100

    updates

--- geomstats/geometry/with_boundary/polytope.py
+++ geomstats/geometry/with_boundary/polytope.py
@@ -215,13 +215,13 @@ class HessianPolytopeMetric(EuclideanMetric):
             dim=dim, default_point_type=default_point_type
         )
 
-    def metric_matrix(self, x, t, z):
+    def metric_matrix(self, x, **kwargs):
         return self.T.T @ gs.diag(self.b[:, None] - self.T @ x.T) @ self.T
 
-    def metric_inverse_matrix(self, x, t, z):
+    def metric_inverse_matrix(self, x, **kwargs):
         return gs.linalg.inv(self.metric_matrix(x))
 
-    def metric_inverse_matrix_sqrt(self, x, t, z):
+    def metric_inverse_matrix_sqrt(self, x, **kwargs):
         return gs.sqrt(self.metric_matrix_inv(x))
 
     def exp(self, tangent_vec, base_point, **kwargs):
